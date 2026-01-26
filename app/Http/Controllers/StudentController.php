56a0b98749fe3b3e5509f83af6a34517812d76d7
ivaaan22 <ivangarciac10@gmail.com>
2026-01-26 16:31:04 +0000

    Nueva API

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json(['students' => $students], 200);
+    }
+}
