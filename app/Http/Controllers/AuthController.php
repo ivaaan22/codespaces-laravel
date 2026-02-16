8d87d59604edc8c9aafed191d283c9aa6563af79
ivaaan22 <ivangarciac10@gmail.com>
2026-02-16 16:32:32 +0000

    Trabajo 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,5 +12,28 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'role' => 'required|string|max:100|in:admin,user',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'role' => $request->get('role'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'data' => $user,
+        ], 201);
+    }
 }
