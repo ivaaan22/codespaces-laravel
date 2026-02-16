a1cbe7c909f30d5225865f89a36b2acdd07cfb62
ivaaan22 <ivangarciac10@gmail.com>
2026-02-16 15:57:39 +0000

    Inicio Register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    //
+}
